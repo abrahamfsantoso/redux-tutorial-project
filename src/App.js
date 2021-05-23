5992cf4878ab18a1345a8336fdff1e2ebbab355b
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-05-23 18:31:55 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+// redux stuff
+
+import { createStore } from "redux";
+import reducer from "./reducer";
+// react-redux - Provider - wraps app, connect-used in component
+import { Provider } from "react-redux";
+
+// store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+function App() {
+  // cart setup
+
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
+  );
+}
+
+export default App;
